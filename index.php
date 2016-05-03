701acfe5c7d57b88dcff662aeac556f3c55fa688
hjiasheng <hjs@zerioi.com>
2016-05-03 16:39:36 +0800

    fix

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
 
 require __DIR__.'/vendor/autoload.php';
-
-use ChengFang\EasyPay\Configuration;
-
-Configuration::testCallStatic();
-Configuration::staticFunction();
-
-$configuration = new Configuration;
-$configuration->normalFunction();
-
-$configuration->staticFunction();
-
-call_user_func_array([$configuration, 'normalFunction'], []);
