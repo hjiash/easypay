700cffda8509c2c25abf8e9778446d698a04b06b
hjiasheng <hjs@zerioi.com>
2016-05-11 11:12:24 +0800

    fix

--- demo.php
+++ demo.php
@@ -28,37 +28,37 @@ function getClientIp() {
  * 微信扫码支付
  * @var [type]
  */
-$order = [
-	'body' => '商品描述',
-	'detail' => '商品详情',
-	'out_trade_no' => createOrderNumber(),
-	'product_id' => 1,
-	'total_fee' => 1,
-	'spbill_create_ip' => getClientIp(),
-	'notify_url' => 'http://test.com',
-];
-$context = new PaymentContext('Strategy\WechatPay\WechatUnifiedPay');
-$context->execute($order);
-$result = $context->getResult();
-$result->getCodeUrl();
+// $order = [
+// 	'body' => '商品描述',
+// 	'detail' => '商品详情',
+// 	'out_trade_no' => createOrderNumber(),
+// 	'product_id' => 1,
+// 	'total_fee' => 1,
+// 	'spbill_create_ip' => getClientIp(),
+// 	'notify_url' => 'http://test.com',
+// ];
+// $context = new PaymentContext('Strategy\WechatPay\WechatUnifiedPay');
+// $context->execute($order);
+// $result = $context->getResult();
+// $result->getCodeUrl();
 
 /**
  * 微信公众号内支付
  * @var [type]
  */
-$order = [
-	'body' => '商品描述',
-	'detail' => '商品详情',
-	'out_trade_no' => createOrderNumber(),
-	'total_fee' => 1,
-	'spbill_create_ip' => getClientIp(),
-	'notify_url' => 'http://test.com',
-	'openid' => 'openid'
-];
-$context = new PaymentContext('Strategy\WechatPay\WechatJsPay');
-$context->execute($order);
-$result = $context->getResult();
-$result->createWebPaymentPackage();
+// $order = [
+// 	'body' => '商品描述',
+// 	'detail' => '商品详情',
+// 	'out_trade_no' => createOrderNumber(),
+// 	'total_fee' => 1,
+// 	'spbill_create_ip' => getClientIp(),
+// 	'notify_url' => 'http://test.com',
+// 	'openid' => 'openid'
+// ];
+// $context = new PaymentContext('Strategy\WechatPay\WechatJsPay');
+// $context->execute($order);
+// $result = $context->getResult();
+// $result->createWebPaymentPackage();
 
 /**
  * 微信APP支付
@@ -81,51 +81,51 @@ $result->createWebPaymentPackage();
 /**
  * 微信支付结果回调
  */
-use \ChengFang\EasyPay\Traits\WechatPayNotify;
+// use \ChengFang\EasyPay\Traits\WechatPayNotify;
 
-$body = file_get_contents('php://input');
-$context = new PaymentContext('Strategy\WechatPay\WechatPayNotify');
-$context->execute($body);
+// $body = file_get_contents('php://input');
+// $context = new PaymentContext('Strategy\WechatPay\WechatPayNotify');
+// $context->execute($body);
 
-try{
-    $result = $context->getResult();
-    /**
-     * result是CompleteOrderResponse的对象
-     * 可以根据微信支付文档，将参数转化成驼峰的形式获取返回值
-     */
+// try{
+//     $result = $context->getResult();
+//     *
+//      * result是CompleteOrderResponse的对象
+//      * 可以根据微信支付文档，将参数转化成驼峰的形式获取返回值
+     
     
-    // $result->getTransactionId();
-    // $result->getOutTradeNo();
+//     // $result->getTransactionId();
+//     // $result->getOutTradeNo();
     
-    // $this->success($message = null);
-    $this->success('信息可以不需要');
-}catch(Exception $e){
-    // $this->fail($message = null);
-    $this->fail('信息可以不需要');
-}
+//     // $this->success($message = null);
+//     $this->success('信息可以不需要');
+// }catch(Exception $e){
+//     // $this->fail($message = null);
+//     $this->fail('信息可以不需要');
+// }
 
 /**
  * 微信红包
  */
-$redPack = [
-    'send_name' => '广州乘方',
-    're_openid' => 'o6FwguKysTyj3zlikp8U8DwHk4XA',
-    'total_amount' => 100,
-    'total_num' => 1,
-    'wishing' => '祝福语',
-    'client_ip' => getClientIp(),
-    'act_name' => '活动名称',
-    'remark' => '备注'
-];
-$context = new PaymentContext('Strategy\WechatPay\WechatRedPack');
-$context->execute($redPack);
-/**
- * result是RedPackResponse的对象
- * 可以根据微信支付文档，将参数转化成驼峰的形式获取返回值
- */
-$result = $context->getResult();
-echo $result->getSendListid();
-exit;
+// $redPack = [
+//     'send_name' => '广州乘方',
+//     're_openid' => 'o6FwguKysTyj3zlikp8U8DwHk4XA',
+//     'total_amount' => 100,
+//     'total_num' => 1,
+//     'wishing' => '祝福语',
+//     'client_ip' => getClientIp(),
+//     'act_name' => '活动名称',
+//     'remark' => '备注'
+// ];
+// $context = new PaymentContext('Strategy\WechatPay\WechatRedPack');
+// $context->execute($redPack);
+// /**
+//  * result是RedPackResponse的对象
+//  * 可以根据微信支付文档，将参数转化成驼峰的形式获取返回值
+//  */
+// $result = $context->getResult();
+// echo $result->getSendListid();
+// exit;
 
 /**
  * 支付宝PC即时到账
@@ -136,7 +136,7 @@ $order = [
 	'body' => '商品详情',
 	'out_trade_no' => createOrderNumber(),
 	'total_fee' => 0.01,
-	'notify_url' => '',
+	'notify_url' => 'http://baidu.com',
 	'return_url' => ''
 ];
 // $context = new PaymentContext('Strategy\Alipay\AlipayPcExpress');
@@ -149,7 +149,9 @@ $order = [
  */
 $context = new PaymentContext('Strategy\Alipay\AlipayWapExpress');
 $context->execute($order);
-echo $context->getResult();
+$result = $context->getResult();
+echo $result->getRedirectUrl();
+exit;
 
 /**
  * 支付宝支付结果回调
